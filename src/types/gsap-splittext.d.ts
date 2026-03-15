f068c148bf155c662c8a7957a6d64ad7bc79aea6
Sujit Sasmal <sujitsasmal40234@gmail.com>
2026-03-15 16:19:03 +0530

    fix gsap splittext types

---
+++ src/types/gsap-splittext.d.ts
@@ -0,0 +1,8 @@
+declare module "gsap-trial/SplitText" {
+  export default class SplitText {
+    constructor(target: any, vars?: any);
+    chars: any[];
+    words: any[];
+    lines: any[];
+  }
+}
